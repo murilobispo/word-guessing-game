6ae12586adf4016ad5be82319a57efedc8e022c3
murilobispo <murilooliveirabispo90@gmail.com>
2025-12-19 02:12:09 -0300

    feat(keyboard): initialize keyboard layout

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ root_width = root.winfo_width()
 
 #root.bind("<Key>", f)
 
-##
 header = tk.Frame(root, background="green", height=50)
 header.pack(fill="x", side="top")
 header.pack_propagate(False)
@@ -40,7 +39,6 @@ h1 = tk.Label(
     anchor="center"
 )
 h1.pack(expand=True)
-##
 
 ##
 main= tk.Frame(root, background="blue")
@@ -50,26 +48,39 @@ guesses = tk.Frame(main, background="darkgray", width=350, height=400)
 guesses.pack()
 guesses.pack_propagate(False)
 
-keyboard = tk.Frame(main, background="red", width=root_width, height=200)
-keyboard.pack(pady=(30,0))
+keyboard = tk.Frame(main, background="red", width=root_width, height=201)
+keyboard.pack(pady=(29,0))
 keyboard.pack_propagate(False)
 
-keys_value = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-              ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-              ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BS']
-                ]
+
+
+keys_value =[   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+                ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BS']
+            ]
+
+keyboard_rows = []
+for i in keys_value:
+    frame = tk.Frame(keyboard)
+    frame.pack(fill="both", expand=True, pady=4)
+    keyboard_rows.append(frame)
+keyboard_rows[1].pack_configure(padx=6)
+
+for i in range(len(keyboard_rows)):
+    for j in range(len(keys_value[i])):
+        lbl = tk.Label(master=keyboard_rows[i],
+                       text=keys_value[i][j],
+                        relief="solid"
+                        
+                       )
+        lbl.pack(side="left", expand=True, fill="both", padx=2)
 
 #for i, row in enumerate(keys_value):
     #row_frame = tk.Frame(keyboard, bg="white")
-    #row_frame.pack(pady=2)
-    #for value in row:
-       # lbl = tk.Label(row_frame, text=value, background="green", width=5, height=2)
-        #lbl.pack(side="left", padx=2)
-
-
-
-        
-##
+    #row_frame.pack()
+    #for j, value in enumerate(row):
+        #lbl = tk.Label(row_frame, text=value, background="green", width=4, height=4)
+       # lbl.grid(row=i, column=j)
 
 ##
 footer= tk.Frame(root, background="orange", height=20)
